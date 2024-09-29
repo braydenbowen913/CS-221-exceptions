8f6037bf834b471f9a054956c4e0520cdb23ae5b
Brayden Bowen <braydenbowen913@u.boisestate.edu>
2024-09-29 15:17:34 -0600

    FormatChecker

--- FormatChecker.java
+++ FormatChecker.java
@@ -1,21 +1,44 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FormatChecker
 {
-    static boolean isValid = false;
-
-    static boolean checkFile(String fileName)
-    {
-
-        return isValid;
-    }
 
     public static void main(String args[]) 
     {
         for (String arg: args)
         {
-            checkFile(arg);
+            File f = new File(arg);
+            int[][] array = new int[1][1];
+
+            try 
+            {
+                Scanner fileScnr = new Scanner(f);
+                String scannedLine = fileScnr.nextLine();
+
+                Scanner lineScnr = new Scanner(scannedLine);
+
+                int width = lineScnr.nextInt();
+                int length = lineScnr.nextInt();
+
+
+
+                fileScnr.close();
+            }
+            catch (FileNotFoundException e)
+            {
+                System.out.println(e.toString());
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println(e.toString());
+            }
+            catch (InputMismatchException e)
+            {
+                System.out.println(e.toString());
+            }
         }
     }
 }
